8b0214267069dbfc50a58583e0a3f21b60cf421f
8Eithel8 <eithel@list.ru>
2022-09-06 18:52:14 +0300

    feat: создан компонент Main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import './Main.css';
+import Promo from '../Promo/Promo.jsx';
+
+
+function Main() {
+    return (
+        <main>
+            <Promo/>
+        </main>
+    );
+
+}
+
+export default Main;

ccbb7e988934bcf6d38ddcde57fe7b30ae2b234b
8Eithel8 <eithel@list.ru>
2022-09-07 17:15:27 +0300

    feat: создан компонент MoviesCardList

---
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -0,0 +1,13 @@
+import MoviesCard from "../MoviesCard/MoviesCard.jsx";
+import './MoviesCardList.css'
+
+function MoviesCardList() {
+    return (
+        <div className="cards">
+            <MoviesCard/>
+        </div>
+
+    );
+}
+
+export default MoviesCardList;

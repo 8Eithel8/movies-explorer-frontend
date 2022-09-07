bcdab03d092a9f4d8c2df459bcfd9683473bcafb
8Eithel8 <eithel@list.ru>
2022-09-07 17:14:02 +0300

    feat: добавлены элементы в хедер

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,17 @@ function Header(props) {
     return (
         <header className="header">
             <img className="header__logo" src={logo} alt="Логотип"/>
-                <div className="header__menu">
-                    <a className="header__link header__button-signin text-btn" onClick={props.onclick}>Регистрация</a>
-                    <button className="header__button text-btn" onClick={props.onclick}>Войти</button>
+                {/*<div className="header__menu">*/}
+                {/*    <a className="header__link header__button-signin text-btn" onClick={props.onclick}>Регистрация</a>*/}
+                {/*    <button className="header__button text-btn" onClick={props.onclick}>Войти</button>*/}
+                {/*</div>*/}
+                <div className="header__menu header__menu_movie">
+                    <div className="header__nav">
+                    <a href='#' className="header__link link" onClick={props.onclick}>Фильмы</a>
+                    <a href='#' className="header__link link" onClick={props.onclick}>Сохранённые фильмы</a>
+                    </div>
+                    <a href='#' className="header__link link header__link_profile text-btn"
+                       onClick={props.onclick}>Аккаунт</a>
                 </div>
         </header>
     );

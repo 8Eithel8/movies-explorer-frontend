1cd51db855171c81c4d5c186be49846e39baf636
8Eithel8 <eithel@list.ru>
2022-09-07 10:57:58 +0300

    feat: создан компонен AboutMe

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import './Main.css';
 import Promo from '../Promo/Promo.jsx';
 import AboutProject from "../AboutProject/AboutProject.jsx";
 import Techs from "../Techs/Techs.jsx";
+import AboutMe from "../AboutMe/AboutMe.jsx";
 
 
 function Main() {
@@ -10,6 +11,7 @@ function Main() {
             <Promo/>
             <AboutProject/>
             <Techs/>
+            <AboutMe/>
         </main>
     );
 

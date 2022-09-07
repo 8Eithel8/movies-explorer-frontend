4edcdf3519839f45de17fb031d7bdf580906536e
8Eithel8 <eithel@list.ru>
2022-09-07 17:18:00 +0300

    feat: создан компонент Movies

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,21 @@
 import './Main.css';
-import Promo from '../Promo/Promo.jsx';
-import AboutProject from "../AboutProject/AboutProject.jsx";
-import Techs from "../Techs/Techs.jsx";
-import AboutMe from "../AboutMe/AboutMe.jsx";
-import Portfolio from "../Portfolio/Portfolio.js";
+// import Promo from '../Promo/Promo.jsx';
+// import AboutProject from "../AboutProject/AboutProject.jsx";
+// import Techs from "../Techs/Techs.jsx";
+// import AboutMe from "../AboutMe/AboutMe.jsx";
+// import Portfolio from "../Portfolio/Portfolio.js";
+import Movies from "../Movies/Movies.jsx";
 
 function Main() {
     return (
         <main>
-            <Promo/>
-            <AboutProject/>
-            <Techs/>
-            <AboutMe/>
-            <Portfolio/>
+            {/*<Promo/>*/}
+            {/*<AboutProject/>*/}
+            {/*<Techs/>*/}
+            {/*<AboutMe/>*/}
+            {/*<Portfolio/>*/}
+
+            <Movies/>
         </main>
     );
 }

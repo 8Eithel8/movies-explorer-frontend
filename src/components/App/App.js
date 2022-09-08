846c0efe61677de29d65d528d4f7e50a6875f1f7
8Eithel8 <eithel@list.ru>
2022-09-08 11:36:48 +0300

    refactor: изменено раширешие файла

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from "../Header/Header.jsx";
 import Main from "../Main/Main.jsx";
-import Footer from "../Footer/Footer.js";
+import Footer from "../Footer/Footer.jsx";
 
 
 function App() {

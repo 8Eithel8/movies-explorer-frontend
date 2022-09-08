f8d57ffb1f8b9d05ea1fc0c7290e8a4297edb1bf
8Eithel8 <eithel@list.ru>
2022-09-08 19:42:39 +0300

    fix: исправлена высота контейнера и добавлен медиа запрос для кнопок

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -8,20 +8,20 @@ function Profile() {
             <h1 className="profile-title title-page">Привет, Анастасия!</h1>
             <form action="" className="profile-form">
                 <div>
-                <fieldset className="profile-form__set">
-                    <label className="profile-form__label  profile-text-field" htmlFor="name">
-                        Имя
-                    </label>
-                    <input className="profile-form__field  profile-text-field" type="text" name="name" id="name"
-                           value="Анастасия" required/>
-                </fieldset>
-                <fieldset className="profile-form__set">
-                    <label className="profile-form__label  profile-text-field" htmlFor="email">
-                        E-mail
-                    </label>
-                    <input className="profile-form__field  profile-text-field" type="text" name="email" id="email"
-                           value="pochta@ya.ru" required/>
-                </fieldset>
+                    <fieldset className="profile-form__set">
+                        <label className="profile-form__label  profile-text-field" htmlFor="name">
+                            Имя
+                        </label>
+                        <input className="profile-form__field  profile-text-field" type="text" name="name" id="name"
+                               value="Анастасия" required/>
+                    </fieldset>
+                    <fieldset className="profile-form__set">
+                        <label className="profile-form__label  profile-text-field" htmlFor="email">
+                            E-mail
+                        </label>
+                        <input className="profile-form__field  profile-text-field" type="text" name="email" id="email"
+                               value="pochta@ya.ru" required/>
+                    </fieldset>
                 </div>
                 <div className="profile-form__buttons">
                     <button className="profile-form__button profile-form__button_edit" type="submit"

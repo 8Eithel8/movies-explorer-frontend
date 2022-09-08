90f8fe16fcc0bad95d0708006a35bcb57a6651ca
8Eithel8 <eithel@list.ru>
2022-09-08 23:45:50 +0300

    fix: убраны лишние компоненты

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,27 +1,18 @@
 import './Main.css';
-// import Promo from '../Promo/Promo.jsx';
-// import AboutProject from "../AboutProject/AboutProject.jsx";
-// import Techs from "../Techs/Techs.jsx";
-// import AboutMe from "../AboutMe/AboutMe.jsx";
-// import Portfolio from "../Portfolio/Portfolio.js";
-// import Movies from "../Movies/Movies.jsx";
-// import Register from "../Register/Register.jsx";
-import Login from "../Login/Login.jsx";
-import Profile from "../Profile/Profile.jsx";
+import Promo from '../Promo/Promo.jsx';
+import AboutProject from "../AboutProject/AboutProject.jsx";
+import Techs from "../Techs/Techs.jsx";
+import AboutMe from "../AboutMe/AboutMe.jsx";
+import Portfolio from "../Portfolio/Portfolio.js";
 
 function Main() {
     return (
         <main>
-            {/*<Promo/>*/}
-            {/*<AboutProject/>*/}
-            {/*<Techs/>*/}
-            {/*<AboutMe/>*/}
-            {/*<Portfolio/>*/}
-
-            {/*<Movies/>*/}
-            {/*<Register/>*/}
-            {/*<Login/>*/}
-            <Profile/>
+            <Promo/>
+            <AboutProject/>
+            <Techs/>
+            <AboutMe/>
+            <Portfolio/>
         </main>
     );
 }

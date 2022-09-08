c21e478a6263fb1740982baeb71b9c24e92565fc
8Eithel8 <eithel@list.ru>
2022-09-08 13:32:54 +0300

    refactor: компонент Logo вынесен в отдельный файл

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import './Header.css';
-import logo from "../../images/logo.svg";
 import Navigation from "../Navigation/Navigation.jsx";
+import Logo from "../Logo/Logo.jsx";
 
 // import {Link, Route, Switch} from "react-router-dom";
 
 function Header(props) {
     return (
         <header className="header">
-            <img className="header__logo" src={logo} alt="Логотип"/>
+            <Logo/>
             <Navigation/>
         </header>
     );

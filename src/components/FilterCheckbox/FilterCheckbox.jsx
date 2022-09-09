9f293e8746e3830a88d6500f5013e8dd31d2e629
8Eithel8 <eithel@list.ru>
2022-09-09 13:02:50 +0300

    fix: исправлено состояние по умолчанию чекбокса

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -4,8 +4,9 @@ import './FilterCheckbox.css';
 function FilterCheckbox() {
     return (
         <div className="filter-group">
-            <input type="checkbox" checked className="filter-group__checkbox" id="checkbox"/>
+            <input type="checkbox" defaultChecked={true} className="filter-group__checkbox" id="checkbox"/>
             <label htmlFor="checkbox" className="filter-group__checkbox-label"></label>
+            <span className="filter-group__background"></span>
             <span className="filter-group__text">Короткометражки</span>
         </div>
     );

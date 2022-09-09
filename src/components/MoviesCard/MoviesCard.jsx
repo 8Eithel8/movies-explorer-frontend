f8dec049a6eb62b872fec485fd4a1ade8b97b16f
8Eithel8 <eithel@list.ru>
2022-09-10 00:19:01 +0300

    feat: добавлено содержание атрибута alt

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -12,7 +12,7 @@ function MoviesCard(props) {
         };
     return (
         <article className="card">
-            <img className="card__image" src={props.image} alt=""/>
+            <img className="card__image" src={props.image} alt={props.title}/>
             <div className="card__footer">
                 <div>
                     <h2 className="card__title">{props.title}</h2>

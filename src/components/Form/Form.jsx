9670e34f76e73f502b61aa128727c23d6832f505
8Eithel8 <eithel@list.ru>
2022-09-12 13:38:51 +0300

    feat: добавлена регаистрация пользователя и валидация форм

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,48 @@
 import './Form.css';
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
+import React from "react";
+import {register} from "../../utils/MainApi.js";
 
 function Form(props) {
+    //обявление состояние полей формы
+    const [values, setValues] = React.useState({});
+    const [errors, setErrors] = React.useState({});
+    const [isValid, setIsValid] = React.useState(false);
+
+    //отслеживает изменение поля и обновляет состояние формы
+    const handleChange = (event) => {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+        setValues({...values, [name]: value});
+        setErrors({...errors, [name]: target.validationMessage });
+        setIsValid(target.closest("form").checkValidity());
+    };
+    const history = useHistory()
+
+    // регистрация пользователя
+    function onSignUp(userData){
+        register(userData).then((res) => {
+            // setInfoToolImage(iconReg);
+            console.log('Вы успешно зарегистрировались!');
+            // setInfoToolMessage('Вы успешно зарегистрировались!');
+            // setIsInfoToolTipOpen(true);
+            history.push('/signin');
+        }).catch(() => {
+            // setInfoToolImage(iconErr);
+            console.log('Что-то пошло не так! Попробуйте ещё раз.');
+            // setInfoToolMessage('Что-то пошло не так! Попробуйте ещё раз.');
+            // setIsInfoToolTipOpen(true);
+        });
+    }
+
+    const onSubmit = (evt) => {
+        evt.preventDefault();
+        onSignUp(values);
+    }
+
     return (
-        <form className="form" name="form-auth">
+        <form className="form" name="form-auth" onSubmit={onSubmit}>
             <div className="form__container">
                 {props.isShowFieldName &&
                     <label className="form__label" htmlFor="name">
@@ -17,7 +56,10 @@ function Form(props) {
                             required
                             minLength="2"
                             maxLength="40"
+                            pattern="[A-Za-zА-Яа-яЁё\s-]+"
+                            onChange={handleChange}
                         />
+                        <span className="form__error name-error">{errors.name || ''}</span>
                     </label>
                 }
                 <label className="form__label" htmlFor="email">
@@ -31,26 +73,34 @@ function Form(props) {
                         required
                         minLength="2"
                         maxLength="40"
+                        onChange={handleChange}
                     />
+                    <span className="form__error name-error">{errors.email || ''}</span>
                 </label>
-                <label className="form__label" htmlFor="pass">
+                <label className="form__label" htmlFor="password">
                     Пароль
                     <input
-                        className="form__field error"
+                        className="form__field"
                         type="password"
-                        name="pass"
-                        id="pass"
+                        name="password"
+                        id="password"
                         placeholder="Ваш пароль"
                         required
-                        minLength="2"
+                        minLength="8"
                         maxLength="40"
+                        onChange={handleChange}
                     />
-                    <span className="form__error name-error">Что-то пошло не так...</span>
+                    <span className="form__error name-error">{errors.password || ''}</span>
                 </label>
             </div>
             <div>
-                <button className="form__button" type="submit"
-                        aria-label="Редактировать">{props.buttonText}
+                <button
+                    className="form__button"
+                    type="submit"
+                    aria-label="Редактировать"
+                    disabled={!isValid}
+                >
+                    {props.buttonText}
                 </button>
                 <p className="form__text">{props.questionText}
                     <Link className="form__text form__text_color link"  to={props.linkPath}>{props.linkText}</Link>
---
+++ src/utils/MainApi.js
@@ -0,0 +1,42 @@
+export const BASE_URL = 'https://api.ulitina.movies.nomorepartiesxyz.ru';
+
+function responseCheck(res) {
+    if (res.ok) {
+        return res.json();
+    }
+    return Promise.reject(res.status);
+}
+
+export const register = (userData) => {
+    return fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    }).then(responseCheck);
+};
+
+// export const authorize = (email, password) => {
+//     return fetch(`${BASE_URL}/signin`, {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ email, password })
+//     }).then(responseCheck);
+// };
+//
+// export const getData = (token) => {
+//     return fetch(`${BASE_URL}/users/me`, {
+//         method: 'GET',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json',
+//             'Authorization': `Bearer ${token}`,
+//         }
+//     })
+//         .then(responseCheck);
+// }

2dc3bfb9263ea3a9ff1259b56fd67deb7490e64c
8Eithel8 <eithel@list.ru>
2022-09-14 13:06:38 +0300

    feat: добавлена валидация полей профайла

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,27 @@
 import './Profile.css';
-import {Link} from "react-router-dom";
 import React from "react";
+import {CurrentUserContext} from "../../contexts/CurrentUserContext.js";
+import {useFormWithValidation} from "../../utils/FormValidator.js";
 
 function Profile(props) {
+    const currentUser = React.useContext(CurrentUserContext);
+    // const [name, setName] = React.useState(currentUser.name);
+    // const [email , setEmail ] = React.useState(currentUser.email);
+    const { values, setValues, handleChange, errors, isValid, resetForm } = useFormWithValidation();
+
+    React.useEffect(() => {
+        setValues({
+            name: currentUser.name,
+            email: currentUser.email,
+        })
+    }, [currentUser]);
+
     return (
         <main className="profile">
-            <h1 className="profile-title title-page">Привет, Анастасия!</h1>
+            <h1 className="profile-title title-page">Привет, {currentUser.name}!</h1>
             <form action="" className="profile-form">
                 <div>
+                    <span className="profile__error">{ errors.name || '' }</span>
                     <fieldset className="profile-form__set">
                         <label className="profile-form__label  profile-text-field" htmlFor="name">
                             Имя
@@ -18,10 +32,12 @@ function Profile(props) {
                             name="name"
                             id="name"
                             placeholder="Имя"
-                            defaultValue="Анастасия"
+                            value={values.name ?? ""}
                             required
                             minLength="2"
                             maxLength="40"
+                            pattern="[A-Za-zА-Яа-яЁё\s-]+"
+                            onChange={ handleChange }
                         />
                     </fieldset>
                     <fieldset className="profile-form__set">
@@ -30,16 +46,18 @@ function Profile(props) {
                         </label>
                         <input
                             className="profile-form__field  profile-text-field"
-                            type="text"
+                            type="email"
                             name="email"
                             id="email"
                             placeholder="E-mail"
-                            defaultValue="pochta@ya.ru"
+                            value={values.email ?? ""}
                             required
                             minLength="2"
                             maxLength="40"
+                            onChange={ handleChange }
                         />
                     </fieldset>
+                    <span className="profile__error">{ errors.email || '' }</span>
                 </div>
                 <div className="profile-form__buttons">
                     <button className="profile-form__button profile-form__button_edit" type="submit"

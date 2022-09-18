43b8e7af18b17407472904c77c687451d8aea56e
8Eithel8 <eithel@list.ru>
2022-09-18 17:10:55 +0300

    feat: добавлены ссылки и стили карточкам фильмов

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -13,16 +13,18 @@ function MoviesCard(props) {
             label: 'Нравится'
         };
     return (
-        <article className="card">
-            <img className="card__image" src={props.image} alt={props.title}/>
-            <div className="card__footer">
-                <div>
-                    <h2 className="card__title">{props.title}</h2>
-                    <span className="card__duration">{props.duration}</span>
+        <a className="card" href='#' target='_blank'>
+            <article>
+                <img className="card__image" src={props.image} alt={props.title}/>
+                <div className="card__footer">
+                    <div>
+                        <h2 className="card__title">{props.title}</h2>
+                        <span className="card__duration">{props.duration}</span>
+                    </div>
+                    <button className={buttonValue.class} type="button" aria-label={buttonValue.label}></button>
                 </div>
-                <button className={buttonValue.class} type="button" aria-label={buttonValue.label}></button>
-            </div>
-        </article>
+            </article>
+        </a>
     );
 }
 

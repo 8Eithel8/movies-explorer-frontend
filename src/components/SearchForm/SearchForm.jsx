778684971d4b516cd43f3b5b1dccaa1af41fccb7
8Eithel8 <eithel@list.ru>
2022-09-19 15:57:14 +0300

    refactor: компонент SearchForm

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,17 +2,17 @@ import './SearchForm.css';
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox.jsx";
 import React from "react";
 
-function SearchForm(props) {
+function SearchForm({params, onSubmit, inputHandler, checkboxHandler}) {
 
     const [errorMessage, setErrorMessage] = React.useState('');
 
-    const onSubmit = (evt) => {
+    const submitHandler = (evt) => {
         evt.preventDefault();
-        props.onSubmit(setErrorMessage);
+        onSubmit(setErrorMessage);
     }
 
-    const inputHandler = (evt) => {
-        props.inputHandler(evt.target.value);
+    const textHandler = (evt) => {
+        inputHandler(evt.target.value);
         setErrorMessage('');
     }
 
@@ -20,20 +20,20 @@ function SearchForm(props) {
         <section className="search">
             <p className="search__error">{errorMessage}</p>
             <div className="search__container">
-                <form className="search__form" name="films-search" onSubmit={ onSubmit }>
+                <form className="search__form" name="films-search" onSubmit={ submitHandler }>
                     <label htmlFor="checkbox" className="search__field-label">
                         <span className="search__icon"></span>
                         <input
                             className="search__field"
-                            value={props.params.text}
-                            onChange={inputHandler}
+                            value={params.text}
+                            onChange={textHandler}
                             type="text"
                             placeholder="Фильм"
                         />
                     </label>
                     <button type="submit" className="search__button link"></button>
                 </form>
-                <FilterCheckbox checked={props.params.isShorts} changeHandler={props.checkboxHandler}/>
+                <FilterCheckbox checked={params.isShorts} changeHandler={checkboxHandler}/>
             </div>
         </section>
     );

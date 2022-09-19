52b23ce9c1da108025b7dc0cff4c6bf2bd17f87e
8Eithel8 <eithel@list.ru>
2022-09-19 15:53:23 +0300

    refactor: добавлены новые константы

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,26 +1,27 @@
-const BAD_REQ_ERR_CODE = 400;
-const UNAUTH_ERR_CODE = 401;
-const FORBIDDEN_ERR_CODE = 403;
-const NOT_FOUND_ERR_CODE = 404;
-const CONFLICT_ERR_CODE = 409;
-const SERVER_ERR_CODE = 500;
+export const BAD_REQ_ERR_CODE = 400;
+export const UNAUTH_ERR_CODE = 401;
+export const FORBIDDEN_ERR_CODE = 403;
+export const NOT_FOUND_ERR_CODE = 404;
+export const CONFLICT_ERR_CODE = 409;
+export const SERVER_ERR_CODE = 500;
 
-const SERVER_ERR_MSG = 'Что-то пошло не так! Попробуйте ещё раз.';
-const FIELDS_ERR_MSG = 'Некорректное значение одного или нескольких полей';
-const LOGIN_ERR_MSG = 'Неверный логин или пароль.';
-const USER_EXISTS_ERR_MSG = 'Пользователь с таким логином уже зарегистрирован.';
-const SUCCESS_UPDATE_MSG = 'Данные успешно обновлены!'
-const MOVIES_ERR_MSG = 'Во время запроса произошла ошибка. ' +
+export const SERVER_ERR_MSG = 'Что-то пошло не так! Попробуйте ещё раз.';
+export const FIELDS_ERR_MSG = 'Некорректное значение одного или нескольких полей';
+export const LOGIN_ERR_MSG = 'Неверный логин или пароль.';
+export const USER_EXISTS_ERR_MSG = 'Пользователь с таким логином уже зарегистрирован.';
+export const SUCCESS_UPDATE_MSG = 'Данные успешно обновлены!'
+export const MOVIES_ERR_MSG = 'Во время запроса произошла ошибка. ' +
     'Возможно, проблема с соединением или сервер недоступен. ' +
     'Подождите немного и попробуйте ещё раз';
-const MOVIES_NOT_FOUND = 'Ничего не найдено';
-const SEARCH_TEXT_ERR = 'Нужно ввести ключевое слово';
+export const MOVIES_NOT_FOUND = 'Ничего не найдено';
+export const SEARCH_TEXT_ERR = 'Нужно ввести ключевое слово';
 
-const MOVIES_LS_KEY = "movies"
+export const SEARCH_PARAMS_LS_KEY = 'searchParams'
+export const JWT_LS_KEY = 'jwt'
 
-const SHORT_MOVIE_DURATION = 40;
+export const SHORT_MOVIE_DURATION = 40;
 
-const MOVIES_GRID = {
+export const MOVIES_GRID = {
   desk: {
     init: 12,
     add: 4,
@@ -41,26 +42,3 @@ const MOVIES_GRID = {
     maxWidth: 767,
   },
 }
-
-module.exports = {
-  BAD_REQ_ERR_CODE,
-  UNAUTH_ERR_CODE,
-  FORBIDDEN_ERR_CODE,
-  NOT_FOUND_ERR_CODE,
-  CONFLICT_ERR_CODE,
-  SERVER_ERR_CODE,
-
-  SERVER_ERR_MSG,
-  FIELDS_ERR_MSG,
-  LOGIN_ERR_MSG,
-  USER_EXISTS_ERR_MSG,
-  SUCCESS_UPDATE_MSG,
-  MOVIES_ERR_MSG,
-  MOVIES_NOT_FOUND,
-  SEARCH_TEXT_ERR,
-
-  MOVIES_LS_KEY,
-
-  MOVIES_GRID,
-  SHORT_MOVIE_DURATION,
-};

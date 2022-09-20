7dd12a850a4912692c7d39b0751e879b4e8e623d
8Eithel8 <eithel@list.ru>
2022-09-20 12:28:40 +0300

    refactor: удаление не нужного

--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -2,7 +2,7 @@ import {responseCheck} from "./MainApi.js";
 
 export const MOVIES_URL = 'https://api.nomoreparties.co';
 
-export const getMovies = (token) => {
+export const getMovies = () => {
     return fetch(MOVIES_URL + '/beatfilm-movies', { method: 'GET' })
         .then(responseCheck)
         .then(data => data.map(movie => ({

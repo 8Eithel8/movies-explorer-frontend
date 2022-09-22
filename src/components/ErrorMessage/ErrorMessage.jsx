a81e16bd9d9b4e397f24eb92d4c80806831f36db
8Eithel8 <eithel@list.ru>
2022-09-22 15:51:31 +0300

    fix: исправлена работа кнопки назад для  зарегистрированного пользователя

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,15 +1,16 @@
 import './ErrorMessage.css';
 import React from "react";
+import {NOT_FOUND_ERR_CODE} from "../../utils/constants.js";
 
 function ErrorMessage(props) {
-    const handleBack = () => props.history.goBack();
+    const handleBack = () => props.history.go(props.isLoggedIn ? -2 : -1);
 
     return (
         <div className="popup">
             <div className="popup__container">
                 <div>
                     <p className="popup__error-message">
-                        <span className="popup__error-code">404</span>
+                        <span className="popup__error-code">{NOT_FOUND_ERR_CODE}</span>
                         Страница не найдена
                     </p>
                 </div>

fe8a1e07c9a5ea6dc8462644e46e07f2935257f4
8Eithel8 <eithel@list.ru>
2022-09-22 12:47:12 +0300

    refactor: удалён неиспользуемый хук

--- src/utils/FormValidator.js
+++ src/utils/FormValidator.js
@@ -1,19 +1,5 @@
 import React, { useCallback } from "react";
 
-//хук управления формой
-export function useForm() {
-    const [values, setValues] = React.useState({});
-
-    const handleChange = (event) => {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        setValues({...values, [name]: value});
-    };
-
-    return { values, handleChange, setValues };
-}
-
 //хук управления формой и валидации формы
 export function useFormWithValidation() {
     const [values, setValues] = React.useState({});
@@ -29,14 +15,5 @@ export function useFormWithValidation() {
         setIsValid(target.closest("form").checkValidity());
     };
 
-    const resetForm = useCallback(
-        (newValues = {}, newErrors = {}, newIsValid = false) => {
-            setValues(newValues);
-            setErrors(newErrors);
-            setIsValid(newIsValid);
-        },
-        [setValues, setErrors, setIsValid]
-    );
-
-    return { values, setValues, handleChange, errors, isValid, resetForm };
+    return { values, setValues, handleChange, errors, isValid };
 }
